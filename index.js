4eeb4a5d25283ae321bb76de33812f0cd1099f25
Sern <sern2001@hotmail.com>
2022-05-11 17:15:06 +0700

    Add js test for using firestore

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, doc, setDoc } from "firebase/firestore"; 
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyASCqxo8pwAMSjfeXriWtSWjaOgf_17sag",
@@ -15,3 +18,28 @@ const app = initializeApp(firebaseConfig);
 
 console.log(app);
 // document.getElementById("test").innerHTML = collection(db, "book");
+const db = getFirestore(app);
+const book = collection(db, "book");
+console.log(book);
+// const citiesRef = collection(db, "cities");
+
+//  setDoc(doc(citiesRef, "SF"), {
+//     name: "San Francisco", state: "CA", country: "USA",
+//     capital: false, population: 860000,
+//     regions: ["west_coast", "norcal"] });
+//  setDoc(doc(citiesRef, "LA"), {
+//     name: "Los Angeles", state: "CA", country: "USA",
+//     capital: false, population: 3900000,
+//     regions: ["west_coast", "socal"] });
+//  setDoc(doc(citiesRef, "DC"), {
+//     name: "Washington, D.C.", state: null, country: "USA",
+//     capital: true, population: 680000,
+//     regions: ["east_coast"] });
+//  setDoc(doc(citiesRef, "TOK"), {
+//     name: "Tokyo", state: null, country: "Japan",
+//     capital: true, population: 9000000,
+//     regions: ["kanto", "honshu"] });
+//  setDoc(doc(citiesRef, "BJ"), {
+//     name: "Beijing", state: null, country: "China",
+//     capital: true, population: 21500000,
+//     regions: ["jingjinji", "hebei"] });
