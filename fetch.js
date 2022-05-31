1521a52e82e94fbbebf8522dd0fc1a2394bf1c6a
Sern <sern2001@hotmail.com>
2022-05-31 14:27:01 +0700

    Add polling func

--- fetch.js
+++ fetch.js
@@ -1,41 +1,56 @@
-const { data } = require("browserslist");
+// const { get } = require("http");
 const https = require("https");
-window.get = async function() {
-  var tmp;
+
+window.get = function() {
     const options = {
         hostname: 'api.netpie.io',
         path: '/v2/device/shadow/data?alias=device3',
         method: 'GET',
         headers: {'Authorization': 'Device bbcf287f-b6e1-442d-8323-1db628d39352:NYMcSAwMK3XWPYT7T6FTNFsv3XBBXwnr'},
       };
-                
-        const req = https.request(options, res => {
-          console.log(`statusCode: ${res.statusCode}`);
-          var buffer = '', data;
-          res.on('data', d => {
-            buffer += d;
-          });
-                    
-          res.on('end', (err) => {
-            // console.log(buffer);
-            // console.log('\n');
+    
 
-            data = JSON.parse(buffer).data;
-            // console.log(data);
-            tmp = data;
-            // return data;
-          });
+      const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`);
+      var buffer = '', data;
+      res.on('data', d => {
+        buffer += d;
+      });
+      
+      res.on('end', (err) => {
+        // console.log(buffer);
+        // console.log('\n');
         
-        }).on('error', error => {
-          console.error(error);
-        });
+      data = JSON.parse(buffer).data;
+      console.log(data);
+      humidity.innerHTML = JSON.stringify(data["Humidity"]);
+      distance.innerHTML = JSON.stringify(data["Distance"]);
+      temperature.innerHTML = JSON.stringify(data["Temperature"]);
+      hasitem.innerHTML = JSON.stringify(data["hasItem"]);
+      haswarn.innerHTML = JSON.stringify(data["hasWarn"]);
       
-      req.end();
-      return tmp;
       
+      
+    });
+    
+  }).on('error', error => {
+    console.error(error);
+  });
+  
+   req.end();
+   
 }
+async function startPolling () {
+  await get();// รอให้ดึงข้อมูลเสร็จก่อน ค่อยรออีก 3 วินาที
+  setTimeout(startPolling, 3000);
+} 
+  
+   
+   
+   
 
-window.click = function() {
-    console.log('click');
-}
-console.log(window.get());
+   
+// window.click = function() {
+//     console.log('click');
+// }
+startPolling();
--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { collection, doc, getDoc } from "firebase/firestore"; 
-import { getStorage, ref } from "firebase/storage";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { collection, doc, getDoc } from "firebase/firestore"; 
+// import { getStorage, ref } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyASCqxo8pwAMSjfeXriWtSWjaOgf_17sag",
-  authDomain: "embed-lab-project.firebaseapp.com",
-  projectId: "embed-lab-project",
-  storageBucket: "embed-lab-project.appspot.com",
-  messagingSenderId: "1019577633582",
-  appId: "1:1019577633582:web:5197e0c2450debd82788bf",
+// const firebaseConfig = {
+//   apiKey: "AIzaSyASCqxo8pwAMSjfeXriWtSWjaOgf_17sag",
+//   authDomain: "embed-lab-project.firebaseapp.com",
+//   projectId: "embed-lab-project",
+//   storageBucket: "embed-lab-project.appspot.com",
+//   messagingSenderId: "1019577633582",
+//   appId: "1:1019577633582:web:5197e0c2450debd82788bf",
   
-};
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-console.log(app);
-// document.getElementById("test").innerHTML = collection(db, "book");
-const db = getFirestore(app);
-async function opendb(){
+// console.log(app);
+// // document.getElementById("test").innerHTML = collection(db, "book");
+// const db = getFirestore(app);
+// async function opendb(){
 
-  const docRef = doc(db, "tmp", "pics");
-  const docSnap = await getDoc(docRef);
-  const data = docSnap.data().pic;
-  console.log(data);
+//   const docRef = doc(db, "tmp", "pics");
+//   const docSnap = await getDoc(docRef);
+//   const data = docSnap.data().pic;
+//   console.log(data);
   
 
-}
-window.opendb = opendb;
+// }
+// window.opendb = opendb;
 
-function openpic(){
-  document.getElementById('status').src = "pic/cloudy.jpg";
-}
-window.openpic = openpic;
+// function openpic(){
+//   document.getElementById('status').src = "pic/cloudy.jpg";
+// }
+// window.openpic = openpic;
 
-function check(){
-  
-}
+
 
           
 
